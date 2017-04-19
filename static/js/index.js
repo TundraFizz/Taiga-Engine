64276fab6c30756ac7751f8c39fe58031bb6805f
Nateeeeee <gamedevnate@gmail.com>
2017-04-18 23:17:07 -0700

    Helper function PIXIPointAdd, PIXIPointScale TaiScene & TaiEntity class velocity member variable delta time

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,47 @@
+function PIXIPointAdd( point0, point1 )
+{
+  return new PIXI.Point(
+    point0.x + point1.x,
+    point0.y + point1.y );
+}
+
+function PIXIPointScale( point, scale )
+{
+  return new PIXI.Point(
+    point.x * scale,
+    point.y * scale );
+}
+
+var TaiScene = function( stage )
+{
+  this.stage = stage;
+  this.entities = [];
+  this.CreateEntity = function( position, velocity, texture )
+  {
+    sprite = new PIXI.Sprite( texture );
+    sprite.position = position;
+    stage.addChild( sprite );
+
+    entity = new TaiEntity();
+    entity.sprite = sprite;
+    entity.velocity = velocity;
+
+    this.entities.push( entity );
+    return entity;
+  }
+}
+
+var TaiEntity = function( texture )
+{
+}
+
+var sean;
+var shawn;
+var shayn;
+var shane;
+
+var lastMsec;
+
 function init() {
   stage = new PIXI.Container();
   renderer = PIXI.autoDetectRenderer(
@@ -7,25 +51,41 @@ function init() {
   );
 
   var farTexture = PIXI.Texture.fromImage("bg-far.png");
-  far = new PIXI.Sprite(farTexture);
-  far.position.x = 0;
-  far.position.y = 0;
-  stage.addChild(far);
-
   var midTexture = PIXI.Texture.fromImage("bg-mid.png");
-  mid = new PIXI.Sprite(midTexture);
-  mid.position.x = 0;
-  mid.position.y = 128;
-  stage.addChild(mid);
+
+  sean = new TaiScene( stage );
+  shawn = new TaiScene( stage );
+  shayn = new TaiScene( stage );
+  shane = new TaiScene( stage );
+
+  sean.CreateEntity(
+    new PIXI.Point( 0, 0 ),
+    new PIXI.Point( 8, 0 ),
+    farTexture );
+
+  sean.CreateEntity(
+    new PIXI.Point( 0, 128 ),
+    new PIXI.Point( 38, 0 ),
+    midTexture );
 
   requestAnimationFrame(update);
+  lastMsec = performance.now();
 }
 
+var lastMsec = null;
+
 function update() {
-  far.position.x -= 0.128;
-  mid.position.x -= 0.64;
+  var currMsec = performance.now();
+  var deltaSecs = ( currMsec - lastMsec ) / 1000;
+  for( let entity of sean.entities )
+  {
+    entity.sprite.position = PIXIPointAdd(
+      entity.sprite.position,
+      PIXIPointScale( entity.velocity, deltaSecs ) );
+  }
 
   renderer.render(stage);
 
+  lastMsec = currMsec;
   requestAnimationFrame(update);
 }
