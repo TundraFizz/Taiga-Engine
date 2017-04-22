953d36ef4b87de3e698ff9c5ec314fd6f337046a
Tundra Fizz <mageleif@yahoo.com>
2017-04-22 00:28:47 -0700

    Update

--- static/js/index.js
+++ static/js/index.js
@@ -36,29 +36,52 @@ function update(){
 
 // ------------------------------------------------------------
 
-var objectContainer = [];
-var screenWidth     = 800;
-var screenHeight    = 600;
-
 function Taiga(){}
 
 Taiga.prototype.Initialize = function(){
-  this.app = new PIXI.Application(screenWidth, screenHeight, {backgroundColor : 0x1099bb});
+  this.textures        = {};
+  this.objectContainer = [];
+  this.screenWidth     = 800;
+  this.screenHeight    = 600;
+
+  this.app = new PIXI.Application(this.screenWidth, this.screenHeight, {backgroundColor : 0x1099bb});
   $("body").prepend(this.app.view);
+  // PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
 
   this.LoadTextures();
   this.CreatePlanet();
+  this.CreateEnemy("enemy1.png", 0.5, -300, -300);
+  this.CreateEnemy("enemy2.png", 1.0, -500, -500);
+  this.CreateEnemy("enemy3.png", 1.5, -700, -700);
+  this.CreateEnemy("enemy4.png", 2.0, -900, -900);
+  this.CreateEnemy("enemy5.png", 2.5, -1100, -1100);
+  this.CreateEnemy("enemy6.png", 3.0, -1300, -1300);
+  this.CreateEnemy("enemy7.png", 3.5, -1500, -1500);
+  this.CreateEnemy("enemy8.png", 4.0, -1700, -1700);
 
   window.requestAnimationFrame(this.Update.bind(this));
 }
 
 Taiga.prototype.LoadTextures = function(){
-  this.textures = [];
-  this.textures.push(PIXI.Texture.fromImage("planet.png"));
+
+  this.LoadTexture("planet.png");
+  this.LoadTexture("enemy1.png");
+  this.LoadTexture("enemy2.png");
+  this.LoadTexture("enemy3.png");
+  this.LoadTexture("enemy4.png");
+  this.LoadTexture("enemy5.png");
+  this.LoadTexture("enemy6.png");
+  this.LoadTexture("enemy7.png");
+  this.LoadTexture("enemy8.png");
+}
+
+Taiga.prototype.LoadTexture = function(textureName){
+  // this.textures.push(PIXI.Texture.fromImage(textureName));
+  this.textures[textureName] = PIXI.Texture.fromImage(textureName);
 }
 
 Taiga.prototype.CreatePlanet = function(){
-  var object = new PIXI.Sprite(this.textures[0]);
+  var object = new PIXI.Sprite(this.textures["planet.png"]);
   object.interactive = true; // Allow object to respond to mouse and touch events
   object.buttonMode = false; // If the hand cursor appears when you mouse over
   object.anchor.set(0.5);    // Center the anchor point
@@ -89,7 +112,22 @@ Taiga.prototype.CreatePlanet = function(){
 
 
   // Add to the stage
-  objectContainer.push(object);
+  this.objectContainer.push(object);
+  this.app.stage.addChild(object);
+}
+
+Taiga.prototype.CreateEnemy = function(tex, scale, x, y){
+  // var object = new PIXI.Sprite(this.textures["planet.png"]);
+  var object = new PIXI.Sprite(this.textures[tex]);
+  object.anchor.set(0.5);    // Center the anchor point
+  object.scale.set(scale);   // Scale
+
+  // move the sprite to its designated position
+  object.x = x;
+  object.y = y;
+
+  // Add to the stage
+  this.objectContainer.push(object);
   this.app.stage.addChild(object);
 }
 
@@ -99,8 +137,8 @@ Taiga.prototype.Update = function(time){
 
   this.app.stage.scale.x -= .001;
   this.app.stage.scale.y -= .001;
-  this.app.stage.position.x = screenWidth/2;
-  this.app.stage.position.y = screenHeight/2;
+  this.app.stage.position.x = this.screenWidth/2;
+  this.app.stage.position.y = this.screenHeight/2;
 
   // this.app.stage.pivot.x = -screenWidth/2;
   // this.app.stage.pivot.y = -screenHeight/2;
@@ -119,75 +157,6 @@ $(document).ready(function(){
   var taiga = new Taiga();
   taiga.Initialize();
 });
-
-function Bunny(){
-  // create our little bunny friend..
-  var bunny = new PIXI.Sprite(texture);
-
-  // enable the bunny to be interactive... this will allow it to respond to mouse and touch events
-  bunny.interactive = true;
-
-  // this button mode will mean the hand cursor appears when you roll over the bunny with your mouse
-  bunny.buttonMode = true;
-
-  // center the bunny's anchor point
-  bunny.anchor.set(0.5);
-
-  // make it a bit bigger, so it's easier to grab
-  bunny.scale.set(3);
-
-  // setup events for mouse + touch using
-  // the pointer events
-  bunny
-      .on('pointerdown', onDragStart)
-      .on('pointerup', onDragEnd)
-      .on('pointerupoutside', onDragEnd)
-      .on('pointermove', onDragMove);
-
-      // For mouse-only events
-      // .on('mousedown', onDragStart)
-      // .on('mouseup', onDragEnd)
-      // .on('mouseupoutside', onDragEnd)
-      // .on('mousemove', onDragMove);
-
-      // For touch-only events
-      // .on('touchstart', onDragStart)
-      // .on('touchend', onDragEnd)
-      // .on('touchendoutside', onDragEnd)
-      // .on('touchmove', onDragMove);
-
-  // move the sprite to its designated position
-  // bunny.x = x;
-  // bunny.y = y;
-
-  // add it to the stage
-  app.stage.addChild(bunny);
-}
-
-function Init(){
-  var app = new PIXI.Application(800, 600, {backgroundColor : 0x1099bb});
-  $("body").prepend(app.view);
-
-  // create a texture from an image path
-  var texture = PIXI.Texture.fromImage("sprite.png");
-
-
-
-  // Scale mode for pixelation
-  // texture.baseTexture.scaleMode = PIXI.SCALE_MODES.NEAREST;
-
-  // mid = new PIXI.Sprite(texture);
-  // mid.position.x = 0;
-  // mid.position.y = 128;
-  // app.addChild(mid);
-
-  // for (var i = 0; i < 10; i++) {
-  //   createBunny(
-  //       Math.floor(Math.random() * app.renderer.width),
-  //       Math.floor(Math.random() * app.renderer.height)
-  //   );
-  // }
-}
 
 function onDragStart(event) {
     // store a reference to the data
