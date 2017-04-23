7004161ac814302ba609160de953e2efb3a46f8d
Nateeeeee <gamedevnate@gmail.com>
2017-04-22 18:59:34 -0700

    Trying out goldfish movement

--- static/js/taimath.js
+++ static/js/taimath.js
@@ -7,57 +7,69 @@ function TaiVec2D( x, y ){
   this.y = y;
 }
 
-function TaiVec2DAdd( vec0, vec1 ){
-  return new TaiVec2D(
-    vec0.x + vec1.x,
-    vec0.y + vec1.y );
+TaiVec2D.prototype.Set = function( x, y ){
+  this.x += x;
+  this.y += y;
 }
 
-function TaiVec2DCopy( dst, src ){
-  dst.x = src.x;
-  dst.y = src.y;
+TaiVec2D.prototype.Add = function( vec ){
+    this.x += vec.x;
+    this.y += vec.y;
 }
 
-function TaiVec2DEquals( vec0, vec1 ){
+TaiVec2D.prototype.Copy = function( vec ){
+  this.x = vec.x;
+  this.y = vec.y;
+}
+
+TaiVec2D.prototype.Equals = function( vec ){
   result
-    = vec0.x == vec1.x
-    && vec0.y == vec1.y;
+    = this.x == vec1.x
+    && this.y == vec1.y;
   return result;
 }
 
-function TaiVec2DQuadrance( vec ){
-  var result = TaiVec2DDot( vec, vec );
+TaiVec2D.prototype.Quadrance = function(){
+  var result = this.dot( this );
   return result;
 }
 
-function TaiVec2DLen( vec ){
-  var quadrance = TaiVec2DQuadrance( vec );
+TaiVec2D.prototype.Len = function(){
+  var quadrance = this.quadrance();
   var result = Math.sqrt( quadrance );
   return result;
 }
 
-function TaiVec2DDot( vec0, vec1 )
-{
+TaiVec2D.prototype.Dot = function( vec ){
   var result
-    = vec0.x * vec1.x
-    + vec0.y * vec1.y;
+    = this.x * vec1.x
+    + this.y * vec1.y;
   return result;
 }
 
-function TaiVec2DScale( vec, scale ){
-  return new TaiVec2D(
-    vec.x * scale,
-    vec.y * scale );
+TaiVec2D.prototype.RotateRads = function( angleRads ) {
+  // [x1] = [c,-s] * [x0]
+  // [y1]   [s, c]   [y0]
+  var c = Math.cos( angleRads );
+  var s = Math.sin( angleRads );
+  var x = c * this.x - s * this.y;
+  var y = s * this.x + c * this.y;
+  this.x = x;
+  this.y = y;
 }
 
-function TaiVec2DAngleBetweenRads( vec0, vec1 ){
-  var numer = TaiVec2DDot( vec0, vec1 );
-  var denom = TaiVec2DLen( vec0 ) * TaiVec2DLen( vec1 );
+TaiVec2D.prototype.Scale = function( scale ){
+    this.x *= scale;
+    this.y *= scale;
+}
+
+TaiVec2D.prototype.AngleBetweenRads = function( vec ){
+  var numer = this.Dot( vec );
+  var denom = this.Len() * vec.Len();
   return Math.acos( numer / denom );
 }
 
-function TaiVec3DCross( u, v )
-{
+function TaiVec3DCross( u, v ) {
   return new TaiVec3D(
     u.y * v.z - u.z * v.y,
     u.z * v.x - u.x * v.z,
@@ -74,18 +86,15 @@ function TaiVec3D( x, y, z ){
 // Tai Math Helpers
 //
 
-function TaiSquare( val )
-{
+function TaiSquare( val ) {
   return val * val;
 }
 
-function TaiToDegrees( radians )
-{
+function TaiToDegrees( radians ) {
   return radians * 180 / Math.PI;
 }
 
-function TaiToRads( degrees )
-{
+function TaiToRads( degrees ) {
   return degrees * Math.PI / 180;
 }
 
@@ -93,8 +102,7 @@ function TaiToRads( degrees )
 // Tai Math <--> PIXI Math interface
 //
 
-function TaiVec2DToPixiPoint( taiVec )
-{
+function TaiVec2DToPixiPoint( taiVec ) {
   return new PIXI.Point( taiVec.x, taiVec.y );
 }
 
